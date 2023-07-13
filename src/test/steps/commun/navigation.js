cae8adb7e8ddcb7c8373ea7cd7e2ac25aaa2dd65
FuchsBastien <83768726+FuchsBastien@users.noreply.github.com>
2023-07-13 16:14:56 +0200

    Update navigation.js

--- src/test/steps/commun/navigation.js
+++ src/test/steps/commun/navigation.js
@@ -4,13 +4,6 @@ const AccederIframe = require('../../support/commun/navigation/accederIframe');
 const AttendreSecondes = require('../../support/commun/navigation/attendreSecondes');
 const DefilerJusquaElement = require('../../support/commun/action/defilerJusquaElement');
 
-Given(/^Je fais defiler jusqu'a l'element "(.*)"$/, async (locateur) => {
-    await DefilerJusquaElement(page, locateur);
-});
-
-Given(/^Je fais defiler jusqu'a l'element "(.*)" dans l'iframe$/, async (locateur) => {
-    await DefilerJusquaElement(iframe, locateur);
-});  
 
 Given(/^Je vais sur l'onglet "(.*)"$/, {timeout: 25000}, async (numOnglet) =>  {
     page = await ChangerOnglet(page, numOnglet)
@@ -23,3 +16,11 @@ Given(/^Je bascule vers l'iframe "(.*)"$/, {timeout: 25000}, async (iframeLocato
 Given(/^J'attend "(.*)" secondes$/, {timeout: 25000}, async (secondesAAttendre) => {
     await AttendreSecondes(page, secondesAAttendre)
 });
+
+Given(/^Je fais defiler jusqu'a l'element "(.*)"$/, async (locateur) => {
+    await DefilerJusquaElement(page, locateur);
+});
+
+Given(/^Je fais defiler jusqu'a l'element "(.*)" dans l'iframe$/, async (locateur) => {
+    await DefilerJusquaElement(iframe, locateur);
+});  
